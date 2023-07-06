dfda82108be7bb5b93648c57f8b52e94babb8cc8
DatboiCantCode <james.sanney4@gmail.com>
2023-07-06 23:10:15 +1000

    commit

--- main.py
+++ main.py
@@ -4,36 +4,36 @@ from tkinter import *
 def button_clicked():
     measure = measure_input.get()
     if radio_state.get() == 1:
-        kilometers = round(int(measure) * 1.609, 2)
+        kilometers = round(float(measure) * 1.609, 2)
         my_label4.config(text=kilometers)
     if radio_state.get() == 2:
-        miles = round(int(measure) / 1.609, 2)
+        miles = round(float(measure) / 1.609, 2)
         my_label4.config(text=miles)
 
 
 window = Tk()
-window.title("Mile to Km Converter")
+window.title("Measurement Converter")
 window.minsize(width=450, height=200)
 window.config(padx=100, pady=50)
 
 # Label - is equal to
-my_label = Label(text="is equal to", font=("Arial", 12))
+my_label = Label(text="is equal to", font=("Arial", 14, "bold"))
 my_label.grid(column=0, row=1)
 
 # Label - Miles
-my_label2 = Label(text="Miles", font=("Arial", 12))
+my_label2 = Label(text="Miles", font=("Arial", 14))
 my_label2.grid(column=2, row=0)
 
 # Label - Km
-my_label3 = Label(text="Km", font=("Arial", 12))
+my_label3 = Label(text="Km", font=("Arial", 14))
 my_label3.grid(column=2, row=1)
 
-# Label - Km Output
-my_label4 = Label(text="0", font=("Arial", 12, "italic"))
+# Label - Output
+my_label4 = Label(text="0", font=("Arial", 14))
 my_label4.grid(column=1, row=1)
 
 # Button - Calculate
-button = Button(text="Calculate", command=button_clicked,font=("Arial", 12, "bold"))
+button = Button(text="Calculate", command=button_clicked,font=("Arial", 14, "bold"))
 button.grid(column=1, row=3)
 
 # Buttons to select Km's or Miles
